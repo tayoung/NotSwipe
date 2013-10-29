52739cf68df1f32fd658501520ae67d877b62d1c
tayoung <tayoung@utexas.edu>
2013-10-28 23:37:08 -0500

    HAHA! This compiles under Unity with no errors!

--- CS/NotSwipeModel.cs
+++ CS/NotSwipeModel.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
 
 class SwipeDictionaryObject : IComparable {
 	// Data
-	public string* word;
+	public string word;
 	public float weight;
 	
 	// Overrides
@@ -26,45 +27,44 @@ public class NotSwipeModel {
 	// Using floats in place of doubles due to the structure of Vector2.
 	public static float[,] mat = new float[300,300];
 	public static float[] wei  = new float[300];
-	public static float angleFromPoints(Vector2 start, Vector2 end);
 	
 	//
-	public static double wlcs(string input, string word) {
+	public static float wlcs(string input, string word) {
 		for(int i=1; i <= word.Length; i++) {
 			for(int j=1; j <= input.Length; j++) {
 				
 				if(input[j-1] == word[i-1])
-					mat[i][j] = Math.Max(mat[i-1][j-1]+wei[j-1], mat[i-1][j]-30);
+					mat[i, j] = Math.Max(mat[i-1, j-1]+wei[j-1], mat[i-1, j]-30);
 				else
-					mat[i][j] = Math.Max(mat[i-1][j], mat[i][j-1]);
+					mat[i, j] = Math.Max(mat[i-1, j], mat[i, j-1]);
 			}
 		}
-		return mat[word.Length][input.Length];
+		return mat[word.Length, input.Length];
 	}
 	
 	
-	// Self-explanatory
-	public static ArrayList<string> findBestMatches(string* input, ArrayList<Vector2> entrancePoints) {
+	// Main algorithm.
+	public static List<string> findBestMatches(string input, List<Vector2> entrancePoints) {
 		int begin = input[0]-'0';
 		int end = input[input.Length]-'0';
 		// I honestly don't know what to do with this since we don't know the file format.
-		string path = string.Format ("/var/swype/English/{0}-{1}.plist", new string[]{begin,end});
-		// here we need to open the file and put it in the array words
-		ArrayList<string> words = new ArrayList();
-		ArrayList<float> weights = findAngleDifferences(findAnglesForPoints(entrancePoints));
+		string path = String.Format("/var/swype/English/{0}-{1}.plist", new int[]{begin,end});
+		// Here we need to open the file and put it in the words List, but see above.
+		List<string> words = new List<string>();
+		List<float> weights = findAngleDifferences(findAnglesForPoints(entrancePoints));
 		
 		for(int i=0; i < weights.Count; i++) {
 			wei[i+1] = weights[i];
 		}
 		wei[0] = 180;
-		wei.Add(180);
-		ArrayList<SwipeDictionaryObject> arr = new ArrayList();
+		wei[weights.Count+1] = 180;
+		List<SwipeDictionaryObject> arr = new List<SwipeDictionaryObject>();
 		
 		for(int i=0; i < words.Count; i++) {
 			string word = words[i];
 			if(word.Equals(input)){
 				SwipeDictionaryObject tmp = new SwipeDictionaryObject();
-				tmp.weight = (words.Count-i) / (words.count*180 + wlcs(input, word));
+				tmp.weight = (words.Count - i) / (words.Count*180 + wlcs(input, word));
 				tmp.word = word;
 				arr.Add(tmp);
 			}
@@ -74,47 +74,57 @@ public class NotSwipeModel {
 		
 		// Return sorted list of results.
 		// TODO: Can probably do this at the same time as the dictionary object construction.
-		ArrayList<string> results = new ArrayList();
+		List<string> results = new List<string>();
 		foreach(SwipeDictionaryObject o in arr){
-			ret.Add(o.word);
+			results.Add(o.word);
 		}
 		
 		return results;
 	}
 	
-	//
-	public static void updatePreference(string* str) {
+	// Updates the preference a user has for a typed word.
+	public static void updatePreference(string str) {
+		int begin = (int) (str[0] - '0');
+		int end = (int) (str[str.Length] - '0');
+		// I honestly don't know what to do with this since we don't know the file format.
+		string path = String.Format ("/var/swype/English/{0}-{1}.plist", new int[]{begin,end});
+		// Here we need to open the file and put it in the words List, but see above.
+		List<string> words = new List<string>();
 		
+		words.Remove(str);
+		words.Insert(0, str);
+		// TODO: Find out WTF this is.
+		//[[NSPropertyListSerialization dataFromPropertyList:words format:NSPropertyListBinaryFormat_v1_0 errorDescription:nil] writeToFile:path atomically:true];
 	}
 	
 	
-	// Helper functions
+	/***************** Helper functions *****************/
 	
 	// Returns list of angles between each adjacent vector.
-	public static ArrayList<float> findAnglesForPoints(ArrayList<Vector2> entrancePoints) {
-		ArrayList<float> results = new ArrayList();
+	private static List<float> findAnglesForPoints(List<Vector2> entrancePoints) {
+		List<float> results = new List<float>();
 		
 		for(int i=1; i < entrancePoints.Count; i++) {
 			Vector2 t1 = entrancePoints[i-1];
 			Vector2 t2 = entrancePoints[i];
-			results.Add(Vector2.Angle(t1,t2));	// Unity library
+			results.Add(Vector2.Angle(t1, t2));	// Unity library
 		}
 		return results;
 	}
 	
 	// Returns a list of all differences in the angles.
-	public static ArrayList<float> findAngleDifferences(ArrayList angles) {
-		ArrayList<float> results = new ArrayList();
+	private static List<float> findAngleDifferences(List<float> angles) {
+		List<float> results = new List<float>();
 		
 		for(int i=1; i < angles.Count; i++ ) {
 			float t1 = angles[i-1];
 			float t2 = angles[i];
-			results.Add(minAngle(t1,t2));
+			results.Add(minAngle(t1, t2));
 		}
 		return results;
 	}
 
-	//
+	// Self-explanatory name.
 	private static float minAngle(float a1, float a2) {
 		float min = Math.Min(a1,a2);
 		float max = Math.Max(a1,a2);
